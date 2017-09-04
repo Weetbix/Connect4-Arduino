bd48f04ec6c5bbe829b9a3d0742075c56a66ab29
John Hannagan <weetbix_@hotmail.com>
2017-09-04 20:55:18 +0200

    Remove unneeded references

--- Board.hpp
+++ Board.hpp
@@ -13,13 +13,13 @@ class Board
         return board;
     }
 
-    void set(const int &x, const int &y, const bool &val)
+    void set(const int x, const int y, const bool val)
     {
         const int index = (x % WIDTH) + (y * HEIGHT);
         board[index] = val;
     }
 
-    bool get(const int &x, const int &y) const
+    bool get(const int x, const int y) const
     {
         // Return false for values outside of the board
         if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT)
